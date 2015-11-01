02ead4246d08ca6a92609cae214916484a5789dd
ajuste <juste.alvaro@gmail.com>
2015-11-01 17:24:32 -0300

    fix

---
+++ index.js
@@ -0,0 +1,14 @@
+require("./lib/boolean");
+require("./lib/date");
+require("./lib/time");
+
+module.exports = {
+  Reflection        : require("./lib/reflection").Reflection,
+  UUID              : require("./lib/uuid").UUID,
+  Stream            : {
+    WriteReadStream : require("./lib/streams").WriteReadStream,
+  },
+  Convert           : require("./lib/convert"),
+  Debug             : require("./lib/debug"),
+  Validator         : require("./lib/validator").Validator
+};
